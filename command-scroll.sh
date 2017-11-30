8d308fd9ff3ab4b9cce23ff4c6124bf6c12a45e2
kiosk <kiosk@debian.ashinfo.com>
2017-11-30 17:04:43 +0530

    modified

--- command-scroll.sh
+++ command-scroll.sh
@@ -3,8 +3,8 @@
 function fetch {
 while [ 0 ]; 
 do wget -qO - http://www.commandlinefu.com/commands/random/plaintext | \
-grep -v questions/comments | \
-grep -v ScriptRock.com |  \
+grep -v "questions/comments" | \
+grep -v "ScriptRock.com" |  \
 pv -q -L 9
 sleep 1s
 done
